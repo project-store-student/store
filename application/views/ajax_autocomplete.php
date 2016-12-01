8365a6375181e3c9921170894e551a764f48b740
อาม <kamontat.apple@gmail.com>
2016-12-01 13:32:10 +0700

    fix autocomplete

--- application/views/ajax_autocomplete.php
+++ application/views/ajax_autocomplete.php
@@ -3,7 +3,7 @@
 <li class="divider"></li>
    <?php   foreach ($T01 as $value) { ?>
   <li class="li-hover">
-                <a class="a" data-id="<?=$value->sm_autoid?>" href="#"><?php echo $value->sm_productname; ?></a>
+                <a class="a" data-id="<?=$value->sm_autoid?>" href="<?php echo base_url("admin/one_product?id=$value->sm_autoid"); ?>"><?php echo $value->sm_productname; ?></a>
             </li>
          <?php   }?>
 <li class="divider"></li>
@@ -11,7 +11,7 @@
 <li class="divider"></li>
    <?php   foreach ($T02 as $value) { ?>
   <li class="li-hover">
-                <a class="a" data-id="<?=$value->sm_autoid?>" href="#"><?php echo $value->sm_productname; ?></a>
+                <a class="a" data-id="<?=$value->sm_autoid?>" href="<?php echo base_url("admin/one_product?id=$value->sm_autoid"); ?>"><?php echo $value->sm_productname; ?></a>
             </li>
          <?php   }?>
 <li class="divider"></li>
@@ -19,7 +19,7 @@
 <li class="divider"></li>
    <?php   foreach ($T03 as $value) { ?>
   <li class="li-hover">
-                <a class="a" data-id="<?=$value->sm_autoid?>" href="#"><?php echo $value->sm_productname; ?></a>
+                <a class="a" data-id="<?=$value->sm_autoid?>" href="<?php echo base_url("admin/one_product?id=$value->sm_autoid"); ?>"><?php echo $value->sm_productname; ?></a>
             </li>
          <?php   }?>
 <li class="divider"></li>
@@ -27,7 +27,7 @@
 <li class="divider"></li>
    <?php   foreach ($T04 as $value) { ?>
   <li class="li-hover">
-                <a class="a" data-id="<?=$value->sm_autoid?>" href="#"><?php echo $value->sm_productname; ?></a>
+                <a class="a" data-id="<?=$value->sm_autoid?>" href="<?php echo base_url("admin/one_product?id=$value->sm_autoid"); ?>"><?php echo $value->sm_productname; ?></a>
             </li>
          <?php   }?>
 <li class="divider"></li>
@@ -35,28 +35,8 @@
 <li class="divider"></li> 
    <?php   foreach ($T05 as $value) { ?>
   <li class="li-hover">
-                <a class="a" data-id="<?=$value->sm_autoid?>" href="#"><?php echo $value->sm_productname; ?></a>
+                <a class="a" data-id="<?=$value->sm_autoid?>" href="<?php echo base_url("admin/one_product?id=$value->sm_autoid"); ?>"><?php echo $value->sm_productname; ?></a>
             </li>
          <?php   }?>
   
 </ui>
-<script>
-    $("a").on('click', function () {
-        var id = $(this).attr("data-id");
-        $.ajax({
-            url: "<?php echo base_url("admin/one_product"); ?>",
-            method: "POST",
-            data: "id=" + id,
-            success: function (response) {
-                $(".content").empty().append(response);
-        $(".search-overlay").hide();
-         $("#auto").empty();
-         $(".dropdown-show").css("height","0px");
-         $('#Search div').hide();
-                    $('#Search .icon-cancel').hide();
-                    $('#Search').css('width', "auto");
-
-            }
-        });
-    });
-</script>
